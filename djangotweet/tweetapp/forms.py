dd891efa77c59e844b8bb3c59173911e218debba
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-06-27 16:02:04 +0300

    .

--- djangotweet/tweetapp/forms.py
+++ djangotweet/tweetapp/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from django.forms import ModelForm
+from tweetapp.models import Tweet
 
 class AddTweetForm(forms.Form):
     nickname_input = forms.CharField(max_length=50, label='Your nickname')
     tweet_input = forms.CharField(max_length=140)
     
+    
+class AddTweetModelForm(ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ['nickname', 'tweet'] 
